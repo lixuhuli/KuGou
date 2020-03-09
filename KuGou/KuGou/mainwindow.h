0404db9b540e8de55078774428d20f31d278ac95
lixuhuli <43337698+lixuhuli@users.noreply.github.com>
2020-03-09 20:46:19 +0800

    创建项目工程

---
+++ KuGou/KuGou/mainwindow.h
@@ -0,0 +1,15 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QDialog>
+
+class MainWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+};
+
+#endif // MAINWINDOW_H
