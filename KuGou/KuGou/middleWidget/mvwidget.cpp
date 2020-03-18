8866e5aaff91edf0fe0937314bad892dedf201be
lixuhuli <43337698+lixuhuli@users.noreply.github.com>
2020-03-18 22:58:16 +0800

    去除编译中的报警

--- KuGou/KuGou/middleWidget/mvwidget.cpp
+++ KuGou/KuGou/middleWidget/mvwidget.cpp
@@ -14,7 +14,7 @@ void MvWidget::InitConnect() {
 
 }
 
-void MvWidget::paintEvent(QPaintEvent* e) {
+void MvWidget::paintEvent(QPaintEvent*) {
     QPainter p(this);
     p.setBrush(Qt::black);
     p.drawRect(0, 0, width(), height());
